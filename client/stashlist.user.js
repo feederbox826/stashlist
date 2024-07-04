28d4a271311542a7408a1a2c4f1c6238b2feabd7
feederbox826 <feederbox826@users.noreply.github.com>
2024-07-04 02:16:10 -0400

    [stashlist] add individual scene ignore, prettier, persist local scenes

--- client/stashlist.user.js
+++ client/stashlist.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         stashlist userscript
 // @namespace    feederbox
-// @version      2.2.3
+// @version      2.2.4
 // @description  Flag scenes in stashbox as ignore or wishlist, and show matches from local stashdb instance if available.
 // @match        https://stashdb.org/*
 // @connect      localhost:9999
@@ -25,6 +25,7 @@
 // force polyfill
 fetch = GM_fetch;
 
+// configuration
 const stashlist_server = GM_getValue("stashlist_server", {
   apikey: "xxxx",
   host: "https://list.feederbox.cc",
@@ -35,13 +36,15 @@ const localStash = GM_getValue("localStash", {
 });
 GM_setValue("example_key", { apikey: "xxxx", host: "" });
 
+// styling
 GM_addStyle(`
 .stashlist {
   border: 5px solid transparent;
   border-radius: 5px;
 }
 .stashlist.match {
-  border-color: green;
+  border-color: green !important;
+  opacity: 1 !important;
 }
 .stashlist.ignore {
   border-color: red;
@@ -89,23 +92,11 @@ let paginationObserved = false;
 let isSearch = location.href.includes("/search/");
 let isScene = location.href.includes("/scenes/");
 let selector = selectorObj.default;
+let ignorePerformers = false;
+let ignoreStudios = false;
 
+// helper functions
 const zip = (a, b) => a.map((k, i) => [k, b[i]]);
-
-function setupPage() {
-  paginationObserved = false;
-  isSearch = location.href.includes("/search/");
-  isScene = location.href.includes("/scenes/");
-  selector = chooseSelector();
-  // add sync
-  const newSync = document.createElement("a");
-  newSync.onclick = cacheLocal;
-  newSync.textContent = "Sync";
-  newSync.classList = "nav-link";
-  newSync.id = "sync";
-  if (document.querySelector(".navbar-nav #sync")) return;
-  document.querySelector(".navbar-nav").append(newSync);
-}
 
 // wait for visible key elements
 function wfke(selector, callback) {
@@ -114,56 +105,6 @@ function wfke(selector, callback) {
   setTimeout(wfke, 100, selector, callback);
 }
 
-function applyBulkMark(sceneIDs, type) {
-  sceneIDs.forEach((id) => {
-    const scene = document.querySelector(`[data-stash-id="${id}"]`);
-    if (!scene) return;
-    scene.classList.add(type);
-    addRemoveButton(scene);
-  });
-}
-
-async function queryLocalScenes(sceneIDs) {
-    const otherClasses = ["ignore", "wish", "history", "filter"];
-    const localIDs = await idbKeyval.getMany(sceneIDs)
-    zip(sceneIDs, localIDs)
-        .filter(i => i[1])
-        .map(i => {
-            const scene = document.querySelector(`[data-stash-id="${i[0]}"]`)
-            addMatch(scene, i[1])
-            scene.classList.add("match")
-            if (otherClasses.some(elem => scene.classList.contains(elem))) {
-                stashlist.modify(i[0], "history")
-                scene.classList.remove(...otherClasses)
-            }
-        })
-}
-
-async function fetchLocal() {
-  const query = `
-      query FindScenes {
-      findScenes( scene_filter: {
-          stash_id_endpoint: { modifier: NOT_NULL }
-      } filter: { per_page: -1 }
-      ) { scenes { id stash_ids { stash_id }
-  }}}`;
-  const idMap = []
-  const idLocal = await gqlClient(localStash, query, {})
-      .then(data => data.findScenes.scenes)
-  idLocal.forEach(scene => idMap.push([scene.stash_ids[0].stash_id, scene.id]))
-  // sync to stashlist
-  return idMap
-}
-
-async function cacheLocal() {
-  const idMap = await fetchLocal()
-  //clear
-  await idbKeyval.clear()
-  // add in bulk
-  idbKeyval.setMany(idMap)
-  console.log("syncing with local")
-  alert(`Synced ${idMap.length} scenes from local stashapp`)
-}
 
 const chooseSelector = () =>
   isSearch
@@ -179,20 +120,84 @@ const getID = (scene) =>
       ? new URL(location.href).pathname.split("/").pop()
       : scene.querySelector(".d-flex a.text-truncate").href.split("/").pop();
 
+// page setup
+function setupPage() {
+  paginationObserved = false;
+  isSearch = location.href.includes("/search/");
+  isScene = location.href.includes("/scenes/");
+  selector = chooseSelector();
+}
+
+// fetching functions
+async function queryLocalScenes(sceneIDs) {
+  const otherClasses = ["ignore", "wish", "history", "filter"];
+  const localIDs = await idbKeyval.getMany(sceneIDs);
+  zip(sceneIDs, localIDs)
+    .filter((i) => i[1])
+    .map((i) => {
+      const scene = document.querySelector(`[data-stash-id="${i[0]}"]`);
+      addMatch(scene, i[1]);
+      scene.classList.add("match");
+      if (otherClasses.some((elem) => scene.classList.contains(elem))) {
+        stashlist.modify(i[0], "history");
+        scene.classList.remove(...otherClasses);
+      }
+    });
+}
+
+async function cacheLocal() {
+  const query = `
+      query FindScenes {
+      findScenes( scene_filter: {
+          stash_id_endpoint: { modifier: NOT_NULL }
+      } filter: { per_page: -1 }
+      ) { scenes { id stash_ids { stash_id }
+  }}}`;
+  const idLocal = await gqlClient(localStash, query, {}).then(
+    (data) => data.findScenes.scenes,
+  );
+  const idMap = idLocal.map(scene => [scene.stash_ids[0].stash_id, scene.id])
+  // sync to stashlist
+  // clear
+  await idbKeyval.clear();
+  // add in bulk
+  idbKeyval.setMany(idMap);
+  console.log("syncing with local");
+}
+
+function applyBulkMark(sceneIDs, type) {
+  sceneIDs.forEach((id) => {
+    const scene = document.querySelector(`[data-stash-id="${id}"]`);
+    if (!scene) return;
+    scene.classList.add(type);
+    addRemoveButton(scene);
+  });
+}
+
+// event running and listeners
 gqlListener.addEventListener("response", async (e) => {
+  if (!ignorePerformers && !ignoreStudios) await setupStashlist();
   if (e.detail.data.queryScenes) {
     console.log("queryScenes received");
     const scenes = e.detail.data.queryScenes.scenes;
-    const ignorePerformers = await stashlist.getlist("ignorePerformer")
-    const ignoreStudios = await stashlist.getlist("ignoreStudio")
     scanGqlFilter(scenes, ignorePerformers, ignoreStudios);
+  } else if (e.detail.data.findScene) {
+    console.log("findScene received");
+    const scene = e.detail.data.findScene;
+    // check if studio or performers are ignored
+    wfke(".scene-info.card", () => {
+      scanGqlFilter([scene], ignorePerformers, ignoreStudios);
+      markSingleCard(scene, ignorePerformers);
+    });
   }
-})
+});
 
 function scanGqlFilter(scenes, ignorePerformers, ignoreStudios) {
   for (const scene of scenes) {
-    const perfMatch = scene.performers.some(p => ignorePerformers.includes(p.performer.id))
-    const studioMatch = ignoreStudios.includes(scene.studio.id)
+    const perfMatch = scene.performers.some((p) =>
+      ignorePerformers.includes(p.performer.id),
+    );
+    const studioMatch = ignoreStudios.includes(scene.studio.id);
     if (perfMatch || studioMatch) {
       const sceneCard = document.querySelector(`[data-stash-id="${scene.id}"]`);
       sceneCard.classList.add("filter");
@@ -200,6 +205,20 @@ function scanGqlFilter(scenes, ignorePerformers, ignoreStudios) {
       if (studioMatch) sceneCard.classList.add("studio");
     }
   }
+}
+
+function markSingleCard(scene, ignorePerformers) {
+  // mark ignored studio and performers in red
+  document.querySelector('h6>a[href^="/studios/"]').style.color = "red";
+  // iterate over matched performers
+  scene.performers
+    .filter((p) => ignorePerformers.includes(p.performer.id))
+    .forEach(
+      (perf) =>
+        (document.querySelector(
+          `a.scene-performer[href="/performers/${perf.performer.id}"]`,
+        ).style.color = "red"),
+    );
 }
 
 function markScenes() {
@@ -219,11 +238,14 @@ function markScenes() {
   });
   // query API for results
   if (stashids.length === 0) return;
-  stashlist.findBulk(stashids).then((results) => {
-    applyBulkMark(results.ignore, "ignore");
-    applyBulkMark(results.wish, "wish");
-    applyBulkMark(results.history, "history")
-  }).then(() => queryLocalScenes(stashids))
+  stashlist
+    .findBulk(stashids)
+    .then((results) => {
+      applyBulkMark(results.ignore, "ignore");
+      applyBulkMark(results.wish, "wish");
+      applyBulkMark(results.history, "history");
+    })
+    .then(() => queryLocalScenes(stashids));
 }
 
 function addButton(scene, type, text, onclick) {
@@ -312,11 +334,18 @@ function observePerformers() {
 function runPage() {
   console.log("runpage");
   setupPage();
+  cacheLocal();
   wfke(selector.cards, markScenes);
   wfke("ul.pagination", observePerformers);
+}
+async function setupStashlist() {
+  ignorePerformers = await stashlist.getlist("ignorePerformer");
+  ignoreStudios = await stashlist.getlist("ignoreStudio");
+  console.log("synced to stashlist");
 }
 // navigation observer
 new MutationObserver(() => runPage()).observe(document.querySelector("title"), {
   childList: true,
 });
 runPage();
+setupStashlist();
