ac3cc8c1211c414b2f12acd898533ec8c4e9871d
feederbox826 <feederbox826@users.noreply.github.com>
2024-12-26 06:01:30 -0500

    add early exit and screening to not clog up hook by 0.008s

--- plugins/stashlist-sync/stashlist-sync.py
+++ plugins/stashlist-sync/stashlist-sync.py
@@ -4,14 +4,11 @@ import sys
 import requests
 import json
 
-stashlist_endpoint = "https://list.feederbox.cc"
 request_s = requests.Session()
+stashlist_endpoint = "https://list.feederbox.cc"
 
-def processScene(s):
-    if len(s['stash_ids']) == 0:
-        log.debug('no scenes to process')
-        return
-    for sid in s['stash_ids']:
+def historyScene(stashids):
+    for sid in stashids:
         # post to api
         res = request_s.post(stashlist_endpoint + "/api/list/add/history" + "?stashid=" + sid['stash_id'])
         if res.status_code != 200:
@@ -37,19 +34,27 @@ def syncall():
         return
 
 json_input = json.loads(sys.stdin.read())
+
+# early exit
+if 'hookContext' in json_input['args']:
+    stashids = json_input['args']['hookContext']['input']['stash_ids']
+    if not stashids:
+        log.error('no stashid added')
+        exit(0)
+
 FRAGMENT_SERVER = json_input["server_connection"]
 stash = StashInterface(FRAGMENT_SERVER)
-
 config = stash.get_configuration()
-stashlist = config["plugins"]["stashlist-sync"]
-request_s.headers.update({"ApiKey": stashlist["stashlist_apikey"]})
+apikey = config["plugins"]["stashlist-sync"]["stashlist_apikey"]
+if not apikey:
+    log.error('No API key found')
+    exit(1)
+request_s.headers.update({"ApiKey": apikey})
 
-if 'mode' in json_input['args']:
+if 'hookContext' in json_input['args']:
+    log.debug('adding history for scene: ' + str(id))
+    historyScene(stashids)
+elif 'mode' in json_input['args']:
     PLUGIN_ARGS = json_input['args']["mode"]
     if 'syncall' in PLUGIN_ARGS:
         syncall()
-elif 'hookContext' in json_input['args']:
-    id=json_input['args']['hookContext']['id']
-    scene=stash.find_scene(id)
-    log.debug('adding history for scene: ' + str(id))
-    processScene(scene)
