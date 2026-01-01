00442bb2393cb9068daf9f25794b413639a25ab1
feederbox826 <feederbox826@users.noreply.github.com>
2026-01-01 10:55:17 -0500

    [plugin][feat] sync from stashdb fingerprints

--- plugins/stashlist-sync/stashlist-sync.py
+++ plugins/stashlist-sync/stashlist-sync.py
@@ -29,9 +29,11 @@ if not stashids and not MODE:
 
 import requests
 from stashapi.stashapp import StashInterface
+from stashapi.stashbox import StashBoxInterface
 
 request_s = requests.Session()
 stashlist_endpoint = "https://list.feederbox.cc"
+STASHDB_ENDPOINT = "https://stashdb.org/graphql"
 
 def historyScene(stashids):
     for sid in stashids:
@@ -42,6 +44,15 @@ def historyScene(stashids):
             log(res.text, 'error')
             return
 
+def bulk_submit(fingerprints: list[str]):
+    data = {"stashids": fingerprints, "type": "history"}
+    res = request_s.post(stashlist_endpoint + '/api/list/add/bulk', json=data)
+    if res.status_code != 200:
+        log('bulk submit failed', 'error')
+        log(res.text, 'error')
+        return
+    log('bulk submit successful', 'info')
+
 def syncall():
     sqlScenes = stash.sql_query(sql="SELECT stash_id FROM scene_stash_ids")
     # https://stackoverflow.com/a/952952
@@ -51,16 +62,17 @@ def syncall():
         for x in xs
     ]
     log(str(len(scenes))+' scenes to sync.')
-    # bulk submit
-    data = {"stashids": scenes, "type": "history"}
-    res = request_s.post(stashlist_endpoint + '/api/list/add/bulk', json=data)
-    if res.status_code != 200:
-        log('bulk submit failed', 'error')
-        log(res.text, 'error')
-        return
-    log('bulk submit successful', 'info')
-    print("{}")
-    exit(0)
+    bulk_submit(scenes)
+
+def sync_stashdb():
+    # grabs fingerprints from stashdb and syncs them into stashlist
+    # grab apikey from stashdb config
+    stashbox_config = stash.get_stashbox_connection(STASHDB_ENDPOINT)
+    stashdb = StashBoxInterface(stashbox_config)
+    matches = stashdb.find_scenes(scene_query={"has_fingerprint_submissions": True, "sort": "CREATED_AT"}, fragment="id")
+    stashids = [str(match['id']) for match in matches]
+    log(f'Found {len(stashids)} scenes with fingerprint submissions in StashDB', 'info')
+    bulk_submit(stashids)
 
 FRAGMENT_SERVER = json_input["server_connection"]
 stash = StashInterface(FRAGMENT_SERVER)
@@ -78,3 +90,6 @@ elif 'mode' in json_input['args']:
     PLUGIN_ARGS = json_input['args']["mode"]
     if 'syncall' in PLUGIN_ARGS:
         syncall()
+    elif 'sync_stashdb' in PLUGIN_ARGS:
+        sync_stashdb()
+print("{}")
